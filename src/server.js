9755d3cec12e472d8177f6d53ad28fc742add1fa
igameroP <121288152+igameroP@users.noreply.github.com>
2023-01-12 13:08:53 +0100

    Config de rutas

--- src/server.js
+++ src/server.js
@@ -9,17 +9,17 @@ function requireHTTPS(req, res, next) {
 const express = require('express');
 const path = require('path');
 const app = express();
-// app.use(express.static('./dist/'));
-app.use(express.static(__dirname, 'dist', {index: false}))
-app.get('', function(req, res) {
-    res.sendFile(path.join(__dirname, 'src', 'index.html'));
-});
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'src', 'index.html'));
-});
-// app.get('/*', function (req, res) {
-//     res.sendFile('src/index.html', { root: './' }
-//     );
+app.use(express.static('./dist/'));
+// app.use(express.static(__dirname, 'dist', {index: false}))
+// app.get('', function(req, res) {
+//     res.sendFile(path.join(__dirname, 'src', 'index.html'));
 // });
+
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname, 'src', 'index.html'));
+// });
+app.get('/*', function (req, res) {
+    res.sendFile('src/index.html', { root: './' }
+    );
+});
 app.listen(process.env.PORT || 8080);
