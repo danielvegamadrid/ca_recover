3e7cdb018701fe3f6a891f97e0869ae73c6395fd
igameroP <121288152+igameroP@users.noreply.github.com>
2023-01-12 13:35:54 +0100

    Config de rutas

--- src/server.js
+++ src/server.js
@@ -23,7 +23,7 @@ const path = require('path');
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'), { root: './' });
+  res.sendFile(path.join('./dist/index.html'), { root: './' });
 });
 
 // app.get('/*', function (req, res) {
